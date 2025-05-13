34fed773ece23290449f27248ea402a1ae245fe2
Bruce Gomes <brucegomestech@gmail.com>
2025-05-13 09:54:46 -0300

    refactor: global style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as S from '../styles/IndexPage.styles';
-import GlobalStyles from '../components/GlobalStyles';
+import GlobalStyles from '../styles/GlobalStyles';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Poll from '../components/Poll';
--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-
   @import url('https://rsms.me/inter/inter.css');
-  html { font-family: 'Inter', sans-serif; }
-  @supports (font-variation-settings: normal) {
-    html { font-family: 'Inter var', sans-serif; }
+
+  html {
+    font-family: 'Inter', sans-serif;
+    @supports (font-variation-settings: normal) {
+      font-family: 'Inter var', sans-serif;
+    }
   }
 
   body {
